452112ff899e74d4a7209dc23c5dc1a188522472
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com>
2023-03-04 00:31:27 -0700

    add v1 paint capability

--- main.js
+++ main.js
@@ -4,8 +4,39 @@ const gridSize = size * size;
 const gridFrame = document.querySelector(".gridFrame");
 for (let i = 0; i < gridSize; i++) {
   const gridSquare = document.createElement("div");
-
   gridFrame.appendChild(gridSquare);
 }
 
 gridFrame.style.cssText = `grid-template-columns: repeat(${size}, auto)`;
+
+let mouseIsDown = false;
+
+document.addEventListener("mousedown", () => {
+  mouseIsDown = true;
+});
+
+document.addEventListener("mouseup", () => {
+  mouseIsDown = false;
+});
+
+const boxes = document.querySelectorAll(".gridFrame > div");
+
+boxes.forEach((box) => {
+  box.addEventListener("mouseover", (e) => {
+    if (!mouseIsDown) {
+      return;
+    }
+    e.target.style.backgroundColor = "green";
+  });
+});
+
+/* gridFrame.children.addEventListener(
+  "mouseover",
+  (e) => {
+    if (!mouseIsDown) {
+      return;
+    }
+    e.target.style.backgroundColor = "green";
+  },
+  
+); */
