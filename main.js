4e3d3f787301740dd3a38cc9f99b8eb8c4f0980a
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com>
2023-03-03 23:49:34 -0700

    add generate grid functionality

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 const size = 16;
-const gridSize = (size * size)
+const gridSize = size * size;
 
+const gridFrame = document.querySelector(".gridFrame");
+for (let i = 0; i < gridSize; i++) {
+  const gridSquare = document.createElement("div");
+
+  gridFrame.appendChild(gridSquare);
+}
+
+gridFrame.style.cssText = `grid-template-columns: repeat(${size}, auto)`;
